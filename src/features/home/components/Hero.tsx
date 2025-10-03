cd239bfb86256186715ba6052699affca2f52d10
yohannestakata <johntakata002@gmail.com>
2025-10-03 16:15:23 +0300

    Update Hero component image source for improved visual quality

--- src/features/home/components/Hero.tsx
+++ src/features/home/components/Hero.tsx
@@ -45,7 +45,7 @@ export function Hero() {
           <div className="relative">
             <div className="rounded-xl border border-neutral-200 bg-white aspect-square shadow-sm overflow-hidden">
               <Image
-                src="https://images.unsplash.com/photo-1732453147369-3144a7cd4b47?q=80&w=1074&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                src="https://images.unsplash.com/photo-1689857538296-b6e1a392a91d?q=80&w=806&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
                 width={500}
                 height={500}
                 alt="Learning preview"
